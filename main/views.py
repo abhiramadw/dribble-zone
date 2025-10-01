c2960a9ed27c5dad2a9a0e91f2dd8343a2a83870
Abhirama <pria.abhirama@ui.ac.id>
2025-10-01 10:58:48 +0700

    Tugas 5 DONE

--- main/views.py
+++ main/views.py
@@ -14,11 +14,17 @@ from django.urls import reverse
 @login_required(login_url='/login')
 def show_main(request):
     filter_type = request.GET.get("filter", "all")
+    category = request.GET.get("category", "")  # ambil kategori dari query string
     
+    # filter all / my product
     if filter_type == "all":
         product_list = Product.objects.all()
     else:
         product_list = Product.objects.filter(user=request.user)
+    
+    # filter berdasarkan kategori
+    if category:
+        product_list = product_list.filter(category__iexact=category)  
     
     context = {
         'npm' : '2406358043',
@@ -111,3 +117,21 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    form = ProductForm(request.POST or None, instance=product)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    product.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
